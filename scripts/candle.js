9f0abcd5937dabc5065bcf4963bbb4312740eb98
NienkeBuursink <nienkebuursink@gmail.com>
2026-06-29 19:41:13 +0200

    design styling

--- scripts/candle.js
+++ scripts/candle.js
@@ -1,27 +1,53 @@
 // cursor
 const glow = document.querySelector(".glow")
 const flame = document.querySelector(".flame")
+const wick = document.querySelector(".wick")
+const slider = document.querySelector("#light-range")
 
-let x = 0
-let y = 0
+let followMode = false;
 
-flame.addEventListener("mousemove", followCursor)
+function setFollowPosition(e) {
+    const x = e.clientX;
+    const y = e.clientY;
 
-function followCursor(){
-    document.addEventListener("mousemove", (e) =>{
-        document.body.style.cursor = "pointer"
-        x = e.clientX
-        y = e.clientY
+    glow.style.position = "fixed";
+    glow.style.left = x + "px";
+    glow.style.top = y + "px";
+    glow.style.transform = "translate(-50%, -50%)";
 
-        glow.style.position= "fixed"
-        glow.style.setProperty("transform", "translate(-50%, -50%)","important")
-        glow.style.left = x + "px"
-        glow.style.top = y + "px"
-
-        flame.style.position= "fixed"
-        flame.style.setProperty("transform", "translate(-50%, -100%)","important")
-        flame.style.left = x + "px"
-        flame.style.top = y + "px"
-        flame.style.pointerEvents = "none"
-    }) 
+    flame.style.position = "fixed";
+    flame.style.left = x + "px";
+    flame.style.top = y + "px";
+    flame.style.transform = "translate(-50%, -100%)";
+    flame.style.pointerEvents = "none";
 }
+
+function resetPosition() {
+    glow.style.position = "";
+    glow.style.left = "";
+    glow.style.top = "";
+    glow.style.transform = "";
+
+    flame.style.position = "";
+    flame.style.left = "";
+    flame.style.top = "";
+    flame.style.transform = "";
+}
+
+wick.addEventListener("mouseenter", () => {
+    if (!followMode) {
+        followMode = true;
+        document.addEventListener("mousemove", setFollowPosition);
+    } else {
+        followMode = false;
+        document.removeEventListener("mousemove", setFollowPosition);
+        resetPosition();
+    }
+});
+
+slider.addEventListener("input", () => {
+    document.documentElement.style.setProperty(
+        "--glow-size",
+        slider.value + "%"
+    );
+});
